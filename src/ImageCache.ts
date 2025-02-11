e85ce57942c1e430debe3ec39e875705eb0b7bc7
Tahvohck <Tahvohck@gmail.com>
2025-02-11 03:43:52 -0500

    Implement local cacheing

--- src/ImageCache.ts
+++ src/ImageCache.ts
@@ -1,8 +1,10 @@
+import * as path from '@std/path';
+import { crypto } from '@std/crypto';
+
 export class LocallyCachedImage {
-    /** Hash of the file contents, SHA-1*/
-    localFileName = 'DEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF';
-    /** Original file name on remote server to avoid downloading multiple times. */
-    remoteFileName = '';
+    /** Local file name. Most likely hash of the file contents, SHA-1 */
+    localFileName = 'DEADBEEFDEADBEEFDEADBEEFDEADBEEFDEADBEEF.png';
+    static readonly cacheLocation = path.join(Deno.cwd(), 'filecache');
 
     /** Save a remote file to the local disk and return the local cache information */
     static async SaveNew(img: Response): Promise<LocallyCachedImage> {
@@ -16,22 +18,42 @@ export class LocallyCachedImage {
         if (type != 'image') {
             throw new Error('File is not an image! Refusing to download');
         }
+        await Deno.mkdir(this.cacheLocation, { recursive: true });
 
-        const tempfilename = await Deno.makeTempFile({
-            prefix: 'superchat-',
-            suffix: `.${subtype}`,
-        });
-        const tempfile = await Deno.open(tempfilename, {
-            write: true,
-            read: true,
-        });
+        const [hashStream, saveStream] = img.body!.tee();
 
-        img.body!.pipeTo(tempfile.writable);
-        tempfile.close();
+        const digestStr = Array.from(
+            new Uint8Array(await crypto.subtle.digest('SHA-1', hashStream)),
+        ).map((byte) => ('0' + byte.toString(16)).slice(-2))
+            .join('')
+            .toUpperCase();
 
-        localcache.remoteFileName = new URL(img.url).pathname;
-        localcache.localFileName = tempfilename;
+        const cachefileName = path.join(this.cacheLocation, `${digestStr}.${subtype}`);
+        localcache.localFileName = cachefileName;
+
+        let tempfile;
+        try {
+            tempfile = await Deno.open(cachefileName, {
+                write: true,
+                read: true,
+                create: true,
+                createNew: true,
+            });
+        } catch {
+            // File already exists, don't write to it.
+            return localcache;
+        }
+
+        await saveStream.pipeTo(tempfile.writable);
+
+        localcache.localFileName = cachefileName;
         //throw new Error("Not yet implented");
         return localcache;
     }
+}
+
+if (import.meta.main) {
+    const foo = await fetch('https://upload.wikimedia.org/wikipedia/en/1/1e/Baseball_%28crop%29.jpg');
+    const lci = await LocallyCachedImage.SaveNew(foo);
+    console.log(lci);
 }
