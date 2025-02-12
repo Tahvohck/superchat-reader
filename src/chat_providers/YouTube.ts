5684a593ad8df51a052876a412765f9007e0f454
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2025-02-12 17:34:28 +0100

    implement basic YouTube donation provider

---
+++ src/chat_providers/YouTube.ts
@@ -0,0 +1,56 @@
+import { DonationMessage, DonationProvider, ProviderConfig } from '@/DonationProvider.ts';
+// this is temporary while I finish up chat support in youtube.js before I push it to npm - Eats
+import { ScrapingClient } from "https://raw.githubusercontent.com/Mampfinator/youtube.js/chat-scraper/src/scraping/ScrapingClient.ts";
+import { ChatClient } from "https://raw.githubusercontent.com/Mampfinator/youtube.js/chat-scraper/src/scraping/ChatClient.ts";
+
+export class YouTubeDonationProvider implements DonationProvider  {
+    name = "YouTube";
+    version = "0.0.1";
+
+    private readonly client: ScrapingClient;
+    private config!: YouTubeConfig;
+
+    constructor() {
+        this.client = new ScrapingClient();
+    }
+    
+    async activate(): Promise<boolean> {
+        try {
+            this.config = await ProviderConfig.load("youtube.json", YouTubeConfig);
+            await this.client.init();
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    deactivate(): boolean {
+        return true;
+    }
+
+    async *process(): AsyncGenerator<DonationMessage> {
+        if (!this.config.streamId) {
+            throw new Error("Stream ID not set.");
+        }
+
+        const chat: ChatClient = await this.client.chat(this.config.streamId!);
+
+        for await (const message of chat.read()) {
+            // TODO: convert to DonationMessage
+            yield this.toDonationMessage(message);
+        }
+    }
+
+    private toDonationMessage(_message: unknown): DonationMessage {
+        return new DonationMessage();
+    }
+
+    configure(): void {
+        // TODO
+    }
+
+}
+
+export class YouTubeConfig extends ProviderConfig {
+    public streamId?: string;
+}
