698814da709b8eaa171df066c6b7373daecaed03
Tahvohck <Tahvohck@gmail.com>
2025-02-17 22:02:40 -0500

    fix typing

--- src/currency_cache.ts
+++ src/currency_cache.ts
@@ -4,9 +4,9 @@ export interface CurrencyAPIResponse {
     documentation: string;
     terms_of_use: string;
     time_last_update_unix: number;
-    time_last_update_utc: Date;
+    time_last_update_utc: string;
     time_next_update_unix: number;
-    time_next_update_utc: Date;
+    time_next_update_utc: string;
     time_eol_unix: number;
     base_code: string;
     rates: Rates;
