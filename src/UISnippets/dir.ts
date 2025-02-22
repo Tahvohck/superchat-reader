11570d5261a22a2cc421466db4b69bfd8bb420eb
Tahvohck <Tahvohck@gmail.com>
2025-02-22 15:31:04 -0500

    Stop tracking essentially 'binary' files

--- src/UISnippets/dir.ts
+++
@@ -1,8 +0,0 @@
-import {E} from "jsr:@nfnitloop/deno-embedder@1.6.1/embed.ts"
-
-export default E({
-  "button.html": () => import("./_button.html.ts"),
-  "checkbox.html": () => import("./_checkbox.html.ts"),
-  "slider.html": () => import("./_slider.html.ts"),
-  "textbox.html": () => import("./_textbox.html.ts"),
-})
