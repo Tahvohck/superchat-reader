514029d1e5f5e84996dab514c1dcacb15c52dbc6
Mampfinator <46071499+Mampfinator@users.noreply.github.com>
2025-02-26 13:47:24 +0100

    allow `SavedConfig` to also trap and save on nested property sets and update the complicated test accordingly. We can't restore classes on load, so that has been removed.

--- src/SavedConfig.ts
+++ src/SavedConfig.ts
@@ -3,6 +3,8 @@ import { join } from '@std/path';
 const SHOULD_SAVE = Symbol('shouldSave');
 export const SAVE_PATH = Symbol('savePath');
 
+const HAS_BEEN_PROXIED = Symbol("hasBeenProxied");
+
 /**
  * Base class for all on-disk automatically saving configs.
  *
@@ -29,35 +31,84 @@ export abstract class SavedConfig {
     protected abstract [SAVE_PATH]: string;
 
     constructor() {
-        return new Proxy(this, {
-            set: (target, prop, value) => {
-                // Enforce CCTOR-only setting of SAVE_PATH
-                if (target[SAVE_PATH] && prop == SAVE_PATH) {
-                    throw new Error("Setting [SAVE_PATH] not allowed outside of cctor")
-                }
-                // Store the current value.
-                const oldvalue = target[prop as keyof typeof target]
-                try {
-                    // Set the value (can't forget to do that)
+        const replacePropertiesWithProxy = <T>(target: T) => {
+            if ((target as { [HAS_BEEN_PROXIED]?: boolean })[HAS_BEEN_PROXIED]) {
+                return;
+            }
+            for (const [key, value] of Object.entries(target as object)) {
+                if (typeof value === "object") {
+                    // I can't think of a better way to cast this, so this will have to do.
+                    // deno-lint-ignore no-explicit-any
+                    (target as any)[key] = new Proxy(value, {
+                        set: setCallback
+                    });
+
+                    replacePropertiesWithProxy(value);
+                } 
+            }
+
+            // since this property is *only* used for preventing duplicate work and should only ever be defined,
+            // never written to, we completely hide and freeze it.
+            Object.defineProperty(target, HAS_BEEN_PROXIED, {
+                value: true,
+                enumerable: false,
+                configurable: false,
+                writable: false,
+            });
+        }
+
+        // deno-lint-ignore no-explicit-any
+        const setCallback = <T>(target: T, prop: string | symbol, value: any): boolean => {
+            // I'm, honestly not sure why this is needed but I assume it's something to do
+            // with child class initializers. Without it, the complicated test case fails,
+            // so I'll just leave it in for now - Eats
+            replacePropertiesWithProxy(target);
+
+            // Enforce CCTOR-only setting of SAVE_PATH
+            if (this[SAVE_PATH] && prop == SAVE_PATH) {
+                throw new Error("Setting [SAVE_PATH] not allowed outside of cctor")
+            }
+            // Store the current value.
+            const oldvalue = target[prop as keyof typeof target];
+            try {
+                // Set the value (can't forget to do that)
+                if (typeof value === "object") {
+                    // The object we're given here was fully instantiated and might itself have object-type properties.
+                    // So we look for those and replace them with our setter Proxy.
+                    replacePropertiesWithProxy(value);
+                    // if the value is an object, we want to trap any sets to it similar to how we trap
+                    // sets on the root object.
+                    target[prop as keyof typeof target] = new Proxy(value, {
+                        set: (target, prop, value) => {
+                            return setCallback(target, prop, value);
+                        }
+                    });
+                } else {
                     target[prop as keyof typeof target] = value;
-                    if (target[SHOULD_SAVE]) {
-                        // Only validate if saving is enabled. If saving is disabled, we're probably operating
-                        // in internal plumbing and a validation pass will happen afterwards.
-                        target.validate()
-                    }
-                } catch (e) {
-                    // Validation failed. Reset to old value, print the error to the console, and return false
-                    target[prop as keyof typeof target] = oldvalue
-                    console.error((e as Error).message)
-                    return false
                 }
-                // Symbol keys can't be persisted to disk, and they're used for internal state tracking as well. So we ignore them as save candidates.
-                if (typeof prop === 'symbol') return true;
-                if (target[SHOULD_SAVE]) target.save();
-                return true;
-            },
+
+                if (this[SHOULD_SAVE]) {
+                    // Only validate if saving is enabled. If saving is disabled, we're probably operating
+                    // in internal plumbing and a validation pass will happen afterwards.
+                    this.validate()
+                }
+            } catch (e) {
+                // Validation failed. Reset to old value, print the error to the console, and return false
+                target[prop as keyof typeof target] = oldvalue
+                console.error((e as Error).message)
+                return false
+            }
+            // Symbol keys can't be persisted to disk, and they're used for internal state tracking as well. So we ignore them as save candidates.
+            if (typeof prop === 'symbol') return true;
+            if (this[SHOULD_SAVE]) this.save();
+            return true;
+        };
+
+        return new Proxy(this, {
+            set: setCallback,
         });
     }
+
 
     /**
      * Save the config to disk at the path specified by `savePath`.
