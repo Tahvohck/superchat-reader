513688c5973ddeeab00b1c9961f9f89894221bcf
Tahvohck <Tahvohck@gmail.com>
2025-03-01 20:23:49 -0500

    add config for window size

--- src/MainConfig.ts
+++ src/MainConfig.ts
@@ -16,13 +16,28 @@ enum LogLevel {
     ALL = 'all',
 }
 
+const MIN_WIDTH = Symbol("MIN WIDTH")
+const MIN_HEIGHT = Symbol("MIN HEIGHT")
 class ProgramConfig extends SavedConfig {
     [SAVE_PATH] = 'mainConfig.json';
+    readonly [MIN_HEIGHT] = 400
+    readonly [MIN_WIDTH] = 600
     debug = false;
     logLevel = LogLevel.INFO;
     public readonly enabledProviders: Record<string, boolean> = {};
+    sizeWidth = 800
+    sizeHeight = 400
 
     override validate(): void {
+        // Non-critical validation failures
+        if (this.sizeWidth < this[MIN_WIDTH]) {
+            this.sizeWidth = this[MIN_WIDTH]
+        }
+        if (this.sizeHeight < this[MIN_HEIGHT]) {
+            this.sizeHeight = this[MIN_HEIGHT]
+        }
+
+        // Critical validation failures
         if (!Object.values(LogLevel).includes(this.logLevel.toLowerCase() as LogLevel)) {
             const options = Object.values(LogLevel).join(', ');
             throw new Error(`Program Config invalid: logLevel must be one of [${options}]`);
