d71bd128c0bf9a7febe16bb33a3898e3acc607e5
Tahvohck <Tahvohck@gmail.com>
2025-03-11 14:43:37 -0400

    swap to using embedded files

--- src/sample-code/cb-panel.ts
+++ src/sample-code/cb-panel.ts
@@ -1,12 +1,43 @@
 import { ConfigurationBuilder } from '@app/ConfigurationBuilder.ts';
 import { WebUI } from 'https://deno.land/x/webui@2.5.3/mod.ts';
 import UISnippets from '@app/UISnippets/dir.ts';
+import { FileHandler } from '@app/FileHandler.ts';
+import { sleep } from '@app/util.ts';
+const win = new WebUI();
+const fh = new FileHandler();
+const cb = new ConfigurationBuilder();
 
 if (import.meta.main) {
-    const win = new WebUI();
-    const builderScript = await (await UISnippets.load('config-custom-elements.html')).text();
+    setUpFH();
+    setUpCB();
 
-    const cb = new ConfigurationBuilder()
+    cb.bind(win);
+    win.setPort(8080);
+    win.setSize(400, 600);
+    win.setFileHandler(fh.handler);
+
+    // We don't care about errors
+    await win.show('embed/cb-panel-sample.html').catch(() => {});
+    await waitForbridge(win);
+    await win.script(`replace('${cb.render()}')`);
+
+    await WebUI.wait();
+    console.log('exit program');
+    Deno.exit();
+}
+
+async function waitForbridge(win: WebUI) {
+    let ready = false;
+    win.bind('ready', () => {
+        ready = true;
+    });
+    while (!ready) {
+        await sleep(25);
+    }
+}
+
+function setUpCB() {
+    cb
         .addButton('click here to boop', {})
         .addButton('Click to exit', {
             callback: () => {
@@ -26,20 +57,31 @@ if (import.meta.main) {
         .addTextBox('Numberbox: blank', { type: 'number' })
         .addTextBox('Numberbox: filled', { type: 'number', value: 1234 })
         .addTextBox('Numberbox: placeholder', { type: 'number', placeholder: 'Enter text here' });
+}
 
-    const html = `<html>
-            <body>
-                ${cb.render()}
-                ${builderScript}
-                <script src="webui.js" defer></script>
-            </body>
-        </html>`;
-    cb.bind(win);
-    win.setSize(400, 600);
-
-    // We don't care about errors
-    win.show(html).catch(() => {});
-
-    await WebUI.wait();
-    console.log('exit program');
+function setUpFH() {
+    fh.router
+        .add('embed/', async (_v, _p, filePath) => {
+            filePath = filePath.slice(1);
+            const data = await UISnippets.get(filePath);
+            if (data) {
+                const ext = filePath.split('.').at(-1);
+                return new Response(await data.text(), {
+                    headers: new Headers({
+                        'Content-Type': `text/${ext}`,
+                    }),
+                });
+            } else {
+                return new Response(null, {
+                    status: 404,
+                    statusText: filePath + ' not a valid embedded file',
+                });
+            }
+        }, true)
+        .add('/favicon.ico', async () => {
+            return await fetch(`http://localhost:8080/favicon.ico`);
+        })
+        .add('/webui.js', async () => {
+            return await fetch(`http://localhost:8080/webui.js`);
+        });
 }
