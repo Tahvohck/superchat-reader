25cb4e265ed0f7293cea0d8130b796f108e74c9f
Tahvohck <Tahvohck@gmail.com>
2025-03-11 19:49:34 -0400

    switch to single quotes

--- UISnippets/custom/elements.js
+++ UISnippets/custom/elements.js
@@ -1,9 +1,10 @@
 let unShadowedStyleSheet
-document.addEventListener("DOMContentLoaded", () => {
-    unShadowedStyleSheet = document.getElementById("config-styles")
-    customElements.define("config-button", ConfigBuilderButton)
-    customElements.define("config-checkbox", ConfigBuilderCheckbox)
-    customElements.define("config-slider", ConfigBuilderSlider)
+document.addEventListener('DOMContentLoaded', () => {
+    unShadowedStyleSheet = document.getElementById('config-styles')
+    customElements.define('config-button', ConfigBuilderButton)
+    customElements.define('config-checkbox', ConfigBuilderCheckbox)
+    customElements.define('config-slider', ConfigBuilderSlider)
+    customElements.define('config-textbox', ConfigBuilderTextbox)
 })
 
 /**
@@ -22,7 +23,7 @@ function getMultipleAttributes(element, ...attributes) {
 class ConfigBuilderButton extends HTMLElement {
     connectedCallback() {
         // get the shadow root prepared with styles
-        const shadow = this.attachShadow({mode: "closed"})
+        const shadow = this.attachShadow({mode: 'closed'})
         const style = new CSSStyleSheet()
         for (const rule of unShadowedStyleSheet.sheet.cssRules) {
             style.insertRule(rule.cssText)
@@ -30,11 +31,11 @@ class ConfigBuilderButton extends HTMLElement {
         shadow.adoptedStyleSheets = [style]
 
         // Now set up the custom element
-        const {uuid, label} = getMultipleAttributes(this, "uuid", "label")
-        const button = document.createElement("button")
+        const {uuid, label} = getMultipleAttributes(this, 'uuid', 'label')
+        const button = document.createElement('button')
 
         button.innerText = label
-        button.setAttribute("id", uuid)
+        button.setAttribute('id', uuid)
         button.addEventListener('click', globalThis[uuid])
         shadow.appendChild(button)
     }
@@ -43,7 +44,7 @@ class ConfigBuilderButton extends HTMLElement {
 class ConfigBuilderCheckbox extends HTMLElement {
     connectedCallback() {
         // get the shadow root prepared with styles
-        const shadow = this.attachShadow({mode: "closed"})
+        const shadow = this.attachShadow({mode: 'closed'})
         const style = new CSSStyleSheet()
         for (const rule of unShadowedStyleSheet.sheet.cssRules) {
             style.insertRule(rule.cssText)
@@ -51,23 +52,23 @@ class ConfigBuilderCheckbox extends HTMLElement {
         shadow.adoptedStyleSheets = [style]
 
         // Now set up the custom element
-        const {uuid, value} = getMultipleAttributes(this, "uuid", "value")
-        const text = this.getAttribute("label")
+        const {uuid, value} = getMultipleAttributes(this, 'uuid', 'value')
+        const text = this.getAttribute('label')
         if (!uuid) {
-            throw new Error("Custom checkbox must have a UUID")
+            throw new Error('Custom checkbox must have a UUID')
         }
-        const label = document.createElement("label")
-        const checkbox = document.createElement("input")
+        const label = document.createElement('label')
+        const checkbox = document.createElement('input')
 
         // Set up label
-        label.setAttribute("for", uuid)
+        label.setAttribute('for', uuid)
         label.innerText = text
         
         // Set up checkbox
-        checkbox.type = "checkbox"
-        checkbox.classList += " checkbox"
+        checkbox.type = 'checkbox'
+        checkbox.classList += ' checkbox'
         checkbox.id = uuid
-        checkbox.checked = value == "true"
+        checkbox.checked = value == 'true'
         checkbox.addEventListener('input', () => {
             globalThis[`checked_${uuid}`](checkbox.checked)
         })
@@ -81,7 +82,7 @@ class ConfigBuilderCheckbox extends HTMLElement {
 class ConfigBuilderSlider extends HTMLElement{
     connectedCallback() {
         // get the shadow root prepared with styles
-        const shadow = this.attachShadow({mode: "closed"})
+        const shadow = this.attachShadow({mode: 'closed'})
         const style = new CSSStyleSheet()
         for (const rule of unShadowedStyleSheet.sheet.cssRules) {
             style.insertRule(rule.cssText)
@@ -92,35 +93,35 @@ class ConfigBuilderSlider extends HTMLElement{
         const {
             uuid, label: labelText,
             min, max, step, value
-        } = getMultipleAttributes(this, "uuid", "label", "min", "max", "step", "value")
+        } = getMultipleAttributes(this, 'uuid', 'label', 'min', 'max', 'step', 'value')
 
         if (!uuid) {
-            throw new Error("Custom slider must have a UUID")
+            throw new Error('Custom slider must have a UUID')
         }
-        const preLabel = document.createElement("label")
-        const slider = document.createElement("input")
-        const valLabel = document.createElement("label")
+        const preLabel = document.createElement('label')
+        const slider = document.createElement('input')
+        const valLabel = document.createElement('label')
 
         // Text label setup
-        preLabel.setAttribute("for", uuid)
-        preLabel.innerText = labelText ?? "NO LABEL"
+        preLabel.setAttribute('for', uuid)
+        preLabel.innerText = labelText ?? 'NO LABEL'
 
         // Slider setup
-        slider.type = "range"
-        slider.classList += "slider"
+        slider.type = 'range'
+        slider.classList += 'slider'
         slider.min = min
         slider.max = max
         slider.step = step
-        slider.setAttribute("value", value)
+        slider.setAttribute('value', value)
         slider.addEventListener('input', () => {
             valLabel.innerText = slider.value
             globalThis[`slider_${uuid}`](slider.value)
         })
 
         // Value label setup
-        valLabel.setAttribute("for", uuid)
+        valLabel.setAttribute('for', uuid)
         valLabel.innerText = value
-        valLabel.classList += " slider-display"
+        valLabel.classList += ' slider-display'
 
         // Add child nodes
         shadow.appendChild(preLabel)
