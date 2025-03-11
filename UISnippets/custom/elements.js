e6727c2c1f88a91c261a11c58c75cea5b9936399
Tahvohck <Tahvohck@gmail.com>
2025-03-11 19:22:49 -0400

    add slider

--- UISnippets/custom/elements.js
+++ UISnippets/custom/elements.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     unShadowedStyleSheet = document.getElementById("config-styles")
     customElements.define("config-button", ConfigBuilderButton)
     customElements.define("config-checkbox", ConfigBuilderCheckbox)
+    customElements.define("config-slider", ConfigBuilderSlider)
 })
 
 /**
@@ -77,4 +78,53 @@ class ConfigBuilderCheckbox extends HTMLElement {
     }
 }
 
+class ConfigBuilderSlider extends HTMLElement{
+    connectedCallback() {
+        // get the shadow root prepared with styles
+        const shadow = this.attachShadow({mode: "closed"})
+        const style = new CSSStyleSheet()
+        for (const rule of unShadowedStyleSheet.sheet.cssRules) {
+            style.insertRule(rule.cssText)
+        }
+        shadow.adoptedStyleSheets = [style]
 
+        // Now set up the custom element
+        const {
+            uuid, label: labelText,
+            min, max, step, value
+        } = getMultipleAttributes(this, "uuid", "label", "min", "max", "step", "value")
+
+        if (!uuid) {
+            throw new Error("Custom slider must have a UUID")
+        }
+        const preLabel = document.createElement("label")
+        const slider = document.createElement("input")
+        const valLabel = document.createElement("label")
+
+        // Text label setup
+        preLabel.setAttribute("for", uuid)
+        preLabel.innerText = labelText ?? "NO LABEL"
+
+        // Slider setup
+        slider.type = "range"
+        slider.classList += "slider"
+        slider.min = min
+        slider.max = max
+        slider.step = step
+        slider.setAttribute("value", value)
+        slider.addEventListener('input', () => {
+            valLabel.innerText = slider.value
+            globalThis[`slider_${uuid}`](slider.value)
+        })
+
+        // Value label setup
+        valLabel.setAttribute("for", uuid)
+        valLabel.innerText = value
+        valLabel.classList += " slider-display"
+
+        // Add child nodes
+        shadow.appendChild(preLabel)
+        shadow.appendChild(valLabel)
+        shadow.appendChild(slider)
+    }
+}
