93d35ebc6b12a232804ef6eeb6ab572a08370c88
Tahvohck <Tahvohck@gmail.com>
2025-03-11 20:16:02 -0400

    add textbox

--- UISnippets/custom/elements.js
+++ UISnippets/custom/elements.js
@@ -129,3 +129,77 @@ class ConfigBuilderSlider extends HTMLElement{
         shadow.appendChild(slider)
     }
 }
+
+class ConfigBuilderTextbox extends HTMLElement {
+    connectedCallback() {
+        // get the shadow root prepared with styles
+        const shadow = this.attachShadow({mode: 'closed'})
+        const style = new CSSStyleSheet()
+        for (const rule of unShadowedStyleSheet.sheet.cssRules) {
+            style.insertRule(rule.cssText)
+        }
+        shadow.adoptedStyleSheets = [style]
+        const {
+            uuid, label, value, placeholder, type
+        } = getMultipleAttributes(this, 'uuid', 'label', 'value', 'placeholder', 'type');
+
+        if (!uuid) {
+            throw new Error('Custom textbox must have a UUID')
+        }
+
+        const labelElement = document.createElement('label');
+        labelElement.htmlFor = uuid;
+        labelElement.innerText = label;
+
+        const input = document.createElement('input');
+        input.setAttribute('value', value);
+        input.placeholder = placeholder;
+        input.type = type;
+
+
+        let timeout;
+        let lastKey = 'Enter';
+        const submit = () => {
+            globalThis[`textbox_${uuid}`](input.value)
+            lastKey = 'Enter';
+        };
+
+        // we don't want to spam the backend with potential save-triggers
+        // so we wait for after the user has stopped typing for a while to actually send the current value.
+        input.addEventListener('input', ev => {
+            clearTimeout(timeout);
+            timeout = setTimeout(submit, 250);
+        });
+
+        // if the user instead hits enter (to signal they're done), we manually submit immediately and
+        // clear our timeout.
+        const ignoreKeys = [
+            'ArrowLeft',
+            'ArrowRight',
+            'ArrowDown',
+            'ArrowUp',
+            'Alt',
+            'Shift',
+            'Control',
+        ]
+        input.addEventListener('keydown', ev => {
+            // ignore various control keys
+            if (ignoreKeys.includes(ev.key)) {
+                return
+            }
+            // submit if key was enter and last noticed key wasn't also enter (prevents spamming)
+            if (ev.key === 'Enter' && lastKey != ev.key) {
+                clearTimeout(timeout);
+                submit()
+            }
+            // don't set last key if it doesn't match the input box type
+            if (!(/\d/.test(ev.key)) && type === 'number') {
+                return
+            }
+            lastKey = ev.key
+        });
+        
+        shadow.appendChild(labelElement);
+        shadow.appendChild(input);
+    }
+}
